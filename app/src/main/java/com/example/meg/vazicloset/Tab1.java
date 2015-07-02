f1035b349756489adc2190673e145bf9d7911d87
munyite-megzs <margaret.sitati1@gmail.com>
2015-07-02 20:01:12 +0300

    added tabs to the appbar and app bar icons too

---
+++ app/src/main/java/com/example/meg/vazicloset/Tab1.java
@@ -0,0 +1,22 @@
+package com.example.meg.vazicloset;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Meg on 2015-07-02.
+ */
+public class Tab1 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.tab_1,container, false);
+        return v;
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/meg/vazicloset/Tab2.java
@@ -0,0 +1,19 @@
+package com.example.meg.vazicloset;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Meg on 2015-07-02.
+ */
+public class Tab2 extends Fragment {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.tab_2,container, false);
+        return v;
+
+    }
+}
